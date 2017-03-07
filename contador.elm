9276ad6d98824205ac084918dff43cc69d5fb37f
rafaellanegro <curso.livre@belasartes.edu.br>
2017-03-07 20:23:36 -0300

    Adicionei vista
    Adicionada vista com botões e contador.

--- contador.elm
+++ contador.elm
@@ -1,5 +1,22 @@
 module Main exposing (..)
 
+import Html exposing (div, button, text)
+
+
+-- Modelo
+
 
 modelo =
     0
+
+
+
+-- Vista
+
+
+main =
+    div []
+        [ button [] [ text "-" ]
+        , text (toString modelo)
+        , button [] [ text "+" ]
+        ]
